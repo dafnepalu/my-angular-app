9fceb27b76b881a6804b7b2802dbb7e109c48111
Dafne Palú <paludafne@gmail.com>
2022-05-31 12:04:38 +0100

    Created a navigation bar, applied basic styles, and generated a couple of components for the pages in the app

---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BreweriesComponent } from './breweries/breweries.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    BreweriesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
